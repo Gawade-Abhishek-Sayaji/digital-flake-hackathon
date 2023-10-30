f44f32faeff078b73695dd19c558d51365c21e5b
Gawade-Abhishek-Sayaji <abhishekgawade1996@gmail.com>
2023-10-30 06:30:02 +0530

    feat/frontend FrontEnd React Initialised with required Components

---
+++ frontend/src/Components/AddCategoryList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddCategoryList() {
+  return (
+    <div>AddCategoryList</div>
+  )
+}
---
+++ frontend/src/Components/LogoutCallout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LogoutCallout() {
+  return (
+    <div>LogoutCallout</div>
+  )
+}
