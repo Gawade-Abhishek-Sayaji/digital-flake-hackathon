eaaed9755043534e0b9f420e1a4d23e99f2fe713
Gawade-Abhishek-Sayaji <abhishekgawade1996@gmail.com>
2023-11-01 05:24:23 +0530

    User Schema and API for Login created

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,17 +1,29 @@
-const express = require('express')
-const app = express()   
-const cors=require('cors')
-require("./db/config")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
 
-app.use(express.static('public'))
-app.use(express.json())
-app.use(cors())
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
 
-app.post('/', (req, res) => {
-    let result = req.body
-    console.log(result)
-    res.send(result)
-})
+app.post("/", async (req, res) => {
+  let user = req.body;
+  if (user.email && user.password) {
+    let result = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    }).select("-password");
+    console.log(result);
+    if (result) {
+      res.send(result);
+    } else {
+      res.send({ message: "Invalid Credentials" });
+    }
+  } else {
+    res.send({ message: "All fields are required" });
+  }
+});
 
-
-app.listen(3000)
+app.listen(5000);
