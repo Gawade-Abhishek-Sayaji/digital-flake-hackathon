7d58da37ba8085fd1004c6b1cff1edf5688475ca
Gawade-Abhishek-Sayaji <abhishekgawade1996@gmail.com>
2023-11-07 17:12:12 +0530

    feat/backend Product related all API service intersected. Api Intersected for Delete and Logout Callout. JWT Token applied and able to send as response while middleware function implemented for checking the token coming from Frontend header

--- backEnd/index.js
+++ backEnd/index.js
@@ -4,48 +4,97 @@ const cors = require("cors");
 require("./db/config");
 const User = require("./db/User");
 const Category=require("./db/Category")
+const Product=require("./db/Product")
+const jwt = require("jsonwebtoken");
+const key="digitalflake";
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
 app.post("/", async (req, res) => {
-  let user = req.body;
-  if (user.email && user.password) {
+  
+  if (req.body.email && req.body.password) {
     let result = await User.findOne({
       email: req.body.email,
       password: req.body.password,
     }).select("-password");
-    console.log(result);
     if (result) {
-      res.send(result);
+      jwt.sign(
+        { result },
+        key,
+        { expiresIn: "2h" },
+        (err, token) => {
+          if (err) {
+            console.log(err);
+            res.send({ message: "Something went wrong" });
+          } else {
+            res.send({ result, token });
+          }
+        }
+      );
     } else {
       res.send({ message: "Invalid Credentials" });
     }
   } else {
     res.send({ message: "All fields are required" });
   }
+  
 });
 
-app.post("/add-category", async (req, res) => {
-  if(req.body.name && req.body.description){
+app.post("/add-category", verifyToken, async (req, res) => {
+  
     let category = new Category(req.body);
     let result = await category.save();
     res.send(result);
-  }else{
-    res.send({message:"All fields are required"})
-  }
+    
 })
 
-app.get("/category-list", async (req, res) => {
+app.get("/category-list", verifyToken, async (req, res) => {
   
   let category = await Category.find();
   res.send(category);
 })
 
-app.delete("/delete-category/:id",async (req, res) => {
+app.delete("/delete-category/:id", verifyToken, async (req, res) => {
   let result = await Category.deleteOne({ _id: req.params.id });
   res.send(result);
 });
 
+app.post("/add-product", verifyToken, async (req, res) => {
+
+  let product = new Product(req.body);
+  let result = await product.save();
+  res.send(result);
+
+})
+
+app.get("/product-list", verifyToken, async (req, res) => {
+
+let product = await Product.find();
+res.send(product);
+})
+
+app.delete("/delete-product/:id", verifyToken, async (req, res) => {
+let result = await Product.deleteOne({ _id: req.params.id });
+res.send(result);
+});
+
+function verifyToken(req, res, next) {
+  let token=req.headers["authorization"];
+  console.log("Inside verifyToken function: ",token);
+  if(token){
+    console.log("true token");
+    token=token.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, key, (err, valid)=>{
+      if(err){
+        res.status(401).send({message:"Invalid Token"});
+      }
+    })
+  }else{
+    res.status(401).send({message:"Please provide token"});
+  }
+  next()
+}
 app.listen(5000);
